56f21b858be2769dbd13f736f087a54c53008445
jonschlinkert <dev@sellside.com>
2014-11-15 06:58:48 -0500

    add recursion

--- index.js
+++ index.js
@@ -10,10 +10,10 @@
 var fs = require('fs');
 var path = require('path');
 
-module.exports = function(dir) {
+module.exports = function(dir, recurse) {
   var o = {};
 
-  fs.readdirSync(dir).forEach(function (name) {
+  walk(dir, recurse).forEach(function (name) {
     var base = path.basename(name, path.extname(name));
     var fp = path.resolve(dir, name);
 
@@ -24,3 +24,35 @@ module.exports = function(dir) {
 
   return o;
 };
+
+
+function walk(dir, recurse) {
+  if (typeof dir !== 'string') {
+    throw new Error('export-files expects a string as the first argument.');
+  }
+
+  var arr = [];
+
+  try {
+    var files = fs.readdirSync(dir);
+    var len = files.length;
+    var i = 0;
+
+    if (Boolean(recurse) === false) {
+      return files.map(function (fp) {
+        return path.join(dir, fp);
+      });
+    }
+
+    while (len--) {
+      var fp = path.join(dir, files[i++]);
+      if (fs.statSync(fp).isDirectory()) {
+        arr.push.apply(arr, walk(fp, recurse));
+      } else {
+        arr = arr.concat(fp);
+      }
+    }
+  } catch(err) {}
+
+  return arr;
+}
