b24cd4c96a9ffca83e1377fce91405abb10c4100
jonschlinkert <jon.schlinkert@sellside.com>
2015-01-21 18:40:53 -0500

    update tests for new features

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /*!
  * export-files <https://github.com/jonschlinkert/export-files>
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Copyright (c) 2014-2015, Jon Schlinkert.
  * Licensed under the MIT License
  */
 
@@ -9,86 +9,121 @@
 
 var path = require('path');
 var should = require('should');
+var exportFiles = require('./');
+
+/**
+ * Support functions
+ */
+
+function rename(fp) {
+  return path.basename(fp);
+}
+
+function filter(re) {
+  return function(fp) {
+    return re.test(fp);
+  }
+}
 
 // path must be absolute for requires to work
 var fixtures = path.join(process.cwd(), 'fixtures');
 
+/**
+ * Tests
+ */
+
 describe('export-files:', function() {
-  it('should recurse when `true` is passed', function(done) {
-    var files = require('./')(fixtures, true);
+  it('should recurse when `true` is passed', function() {
+    var files = exportFiles(fixtures, true);
     files.should.have.properties('a', 'b', 'c', 'd', 'i', 'j', 'k');
-    done();
   });
 
-  it('should return the modules from the given cwd', function(done) {
-    var files = require('./')(fixtures);
+  it('should return the modules from the given cwd', function() {
+    var files = exportFiles(fixtures);
     files.should.have.properties('a', 'b', 'c', 'd');
     files.d.should.have.properties('foo', 'bar', 'baz');
-    done();
   });
 
-  it('should not recurse by default', function(done) {
-    var files = require('./')(fixtures);
+  it('should not recurse by default', function() {
+    var files = exportFiles(fixtures);
     files.should.have.properties('a', 'b', 'c', 'd');
     files.should.not.have.properties('i', 'j', 'k');
-    done();
+  });
+});
+
+describe('callback function', function() {
+  it('should allow a callback function to read files', function() {
+    var files = exportFiles(fixtures, function (fp) {
+      return require(fp)
+    });
+    files.should.have.properties('a', 'b', 'c', 'd');
+    files.should.not.have.properties('i', 'j', 'k');
   });
 
-  describe('custom filters', function() {
-    it('`.css` filter with recurse `true`', function(done) {
-      var opts = {
-        text: true,
-        filter: function(fp) {
-          return /\.css$/.test(path.basename(fp));
-        }
-      };
-      var files = require('./')(fixtures, true, opts);
-      files.should.have.properties('a', 'e');
-      files.a.should.equal('div span {\n  color: red;\n}');
-      files.e.should.equal('#navbar {\n  color: black;\n}');
-      done();
+  it('should use the callback to read files and rename keys from options:', function() {
+    var files = exportFiles(fixtures, {renameKey: rename}, function (fp) {
+      return require(fp);
     });
 
-    it('`.yml` filter with recurse `false`', function(done) {
-      var opts = {
-        yaml: function(fp) {
-          return require('read-yaml').sync(fp)
-        },
-        filter: function(fp) {
-          return /\.yml$/.test(path.basename(fp));
-        }
-      };
-      var files = require('./')(fixtures, false, opts);
+    files.should.have.properties('a.js', 'b.js', 'c.js', 'd.js');
+    files.should.not.have.properties('i.js', 'j.js', 'k.js');
+  });
+});
 
-      files.should.have.properties('a', 'b');
-      files.a.should.have.properties('name','desc');
+describe('custom filters', function() {
+  it('`.css` filter with recurse `true`', function() {
+    var files = exportFiles(fixtures, true, {filter: filter(/\.css$/)});
+    files.should.have.properties('a', 'e');
+    files.a.should.equal('div span {\n  color: red;\n}');
+    files.e.should.equal('#navbar {\n  color: black;\n}');
+  });
 
-      files.b.should.have.property('bb');
-      files.b.bb.should.equal('bbb');
-      done();
+  it('should dynamically choose the reader:', function() {
+    var files = exportFiles(fixtures, false, {
+      read: require('file-reader').file
     });
 
+    files.should.have.properties('a', 'b');
+  });
 
-    it('`.json` filter with recurse `true`', function(done) {
-      var opts = {
-        filter: function(fp) {
-          return /\.json$/.test(path.basename(fp));
-        }
-      };
-      var files = require('./')(fixtures, true, opts);
-
-      files.should.have.properties('a', 'b', 'e', 'k');
-      files.a.should.have.properties('name','desc');
-
-      files.b.should.have.property('bb');
-      files.b.bb.should.equal('bbb');
-
-      files.e.should.have.property('ee');
-      files.e.ee.should.equal('eee');
-
-      files.k.should.have.property('kk');
-      files.k.kk.should.equal('kkk');
-      done();
+  it('should dynamically choose the reader:', function() {
+    var files = exportFiles(fixtures + '/nested', false, {
+      read: require('file-reader').file
     });
+
+    files.should.have.properties('e', 'f', 'g');
+  });
+
+  it('should use a read function and `.yml` filter with recurse `false`', function() {
+    var files = exportFiles(fixtures, false, {
+      read: function(fp) {
+        return require('read-yaml').sync(fp);
+      },
+      filter: filter(/\.ya?ml$/)
+    });
+
+    files.should.have.properties('a', 'b');
+    files.a.should.have.properties('name','desc');
+
+    files.b.should.have.property('bb');
+    files.b.bb.should.equal('bbb');
+  });
+
+  it('should use a `.json` filter with recurse `true`', function() {
+    var files = exportFiles(fixtures, true, {
+      filter: filter(/\.json$/)
+    });
+
+    files.should.have.properties('a', 'b', 'e', 'k');
+    files.a.should.have.properties('name','desc');
+
+    files.b.should.have.property('bb');
+    files.b.bb.should.equal('bbb');
+
+    files.e.should.have.property('ee');
+    files.e.ee.should.equal('eee');
+
+    files.k.should.have.property('kk');
+    files.k.kk.should.equal('kkk');
   });
 });
