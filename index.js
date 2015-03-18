d7c7674aa943b6a755548ffa6d2941d2bb7e7c23
jonschlinkert <jon.schlinkert@sellside.com>
2015-03-18 08:30:10 -0400

    refactored

--- index.js
+++ index.js
@@ -9,220 +9,51 @@
 
 var fs = require('fs');
 var path = require('path');
-var map = require('arr-map')
-var typeOf = require('kind-of');
-var isGlob = require('is-glob');
-var extend = require('extend-shallow');
-var mm = require('micromatch');
 
-/**
- * Expose `exportFiles`
- */
-
-module.exports = exportFiles;
-
-/**
- * Export files from the give `directory` filtering the
- * results with
- *
- * @param  {String} `directory` Use `__dirname` for the immediate directory.
- * @param  {String|Array} `patterns` Glob patterns to use for matching.
- * @param  {Boolean} `recurse` Pass `true` to recurse deeper than the current directory.
- * @param  {String} `options`
- * @param  {Function} `fn` Callback for filtering.
- * @return {String}
- */
-
-function exportFiles(dir, patterns, recurse, options, fn) {
-  if (arguments.length === 1 && !isGlob(dir)) {
-
-    var key = 'exportFiles:' + dir;
-    if (cache.hasOwnProperty(key)) {
-      return cache[key];
-    }
-
-    var result = lookup(dir, false, {}).reduce(function (res, fp) {
-      if (filter(fp, fn)) {
-        res[basename(fp)] = read(fp, fn);
-      }
-      return res;
-    }, {});
-    return (cache[key] = result);
-  }
-  return explode.apply(explode, arguments);
-}
-
-var cache = exportFiles.cache = {};
-
-function explode(dir, patterns, recurse, options, fn) {
-  // don't slice args for v8 optimization
-  var len = arguments.length - 1;
-  var args = new Array(len);
-  for (var i = 0; i < len; i++) {
-    args[i] = arguments[i + 1];
-  }
-
-  var rest = sortArgs(args);
-  patterns = rest['string'] || rest['array'] || rest['regexp'];
-  recurse = rest['boolean'];
-  options = rest['object'];
-  fn = rest['function'];
-
-  var opts = extend({recurse: recurse || false}, options);
-  if (patterns) {
-    opts.filter = function (fp) {
-      return mm.isMatch(fp, patterns);
-    };
-  }
-
-  return lookup(dir, opts.recurse, opts).reduce(function (res, fp) {
-    if (filter(fp, opts, fn)) {
-      res[renameKey(fp, opts)] = read(fp, opts, fn);
-    }
-    return res;
-  }, {});
-}
-
-/**
- * Recursively read directories, starting with the given `dir`.
- *
- * @param  {String} `dir`
- * @param  {Boolean} `recurse` Should the function recurse?
- * @return {Array} Returns an array of files.
- */
-
-function lookup(dir, recurse) {
+module.exports = function exportFiles(dir) {
   if (typeof dir !== 'string') {
-    throw new Error('export-files expects a string as the first argument.');
+    throw new TypeError('export-files expects `dir` to be a string.');
   }
 
-  var key = 'lookup:' + dir + ('' + recurse);
-  if (cache.hasOwnProperty(key)) {
-    return cache[key];
-  }
-
-  var files = fs.readdirSync(dir);
-  var len = files.length;
-  var res = [];
-
-  if (recurse === false) return map(files, resolve(dir));
+  var dirs = tryReaddir(dir);
+  var len = dirs.length;
+  var res = {}
 
   while (len--) {
-    var fp = path.resolve(dir, files[len]);
-    if (isDir(fp)) {
-      res.push.apply(res, lookup(fp, recurse));
-    } else {
-      res.push(fp);
+    var name = dirs[len];
+    var fp = path.resolve(dir, name);
+    if (!fs.statSync(fp).isDirectory() && isValid(fp)) {
+      res[basename(name)] = require(fp);
     }
   }
-  return (cache[key] = res);
+  return res;
+};
+
+function isValid(fp) {
+  return fp.indexOf('index.js') === -1
+    && fp.substr(-3) === '.js';
 }
-
-/**
- * Rename object keys with a custom function.
- * If no function is passed, the basname is
- * returned.
- */
-
-function renameKey(fp, opts) {
-  if (opts && opts.renameKey) {
-    return opts.renameKey(fp, opts);
-  }
-  return basename(fp);
-}
-
-/**
- * Get the basename of a file path.
- */
 
 function basename(fp) {
-  return path.basename(fp, path.extname(fp));
+  return fp.substr(0, fp.length - 3);
 }
 
-/**
- * Read or require the given file with `opts`
- */
-
-function read(fp, opts, fn) {
-  opts = opts || {};
-  opts.encoding = opts.encoding || 'utf8';
-
-  if (opts.read) {
-    return opts.read(fp, opts);
-  } else if (fn) {
-    return fn(fp, opts);
-  }
-
-  if (endsWith(fp, '.js')) {
-    return tryRequire(fp);
-  } else {
-    var str = tryCatch(fs.readFileSync, fp, opts);
-    if (endsWith(fp, '.json')) {
-      return tryCatch(JSON.parse, str);
-    }
-    return str;
-  }
-}
-
-/**
- * Keep try-catch isolated for v8 optimizations
- */
-
-function tryCatch(fn, str, opts) {
+function tryReaddir(fp) {
   try {
-    return fn(str, opts);
-  } catch(err) {}
-  return {};
+    return fs.readdirSync(fp);
+  } catch(err) {
+    err.origin = __dirname;
+    err.msg = 'export-dirs cannot read directory: ' + fp;
+    throw new Error(err);
+  }
 }
 
 function tryRequire(fp) {
   try {
-    return require(path.resolve(fp));
-  } catch(err) {}
-  return null;
-}
-
-/**
- * Return true if `str` ends with `ch`aracters
- */
-
-function endsWith(str, ch) {
-  return str.slice(-ch.length) === ch;
-}
-
-/**
- * Validate (filter) files using the given filter
- * function, falling back on the default filter.
- */
-
-function filter(fp, opts) {
-  if (opts && opts.filter) {
-    return opts.filter(fp, opts);
+    return require(fp);
+  } catch(err) {
+    err.origin = __dirname;
+    err.msg = 'export-dirs cannot require: ' + fp;
+    throw new Error(err);
   }
-  return /[^x]\.js$/.test(fp);
-}
-
-/**
- * Utility functions
- */
-
-function isDir(fp) {
-  return fs.statSync(fp).isDirectory();
-}
-
-function resolve(dir) {
-  return function (fp) {
-    return path.resolve(dir, fp);
-  };
-}
-
-function sortArgs (args) {
-  var len = args.length;
-  var res = {};
-
-  while (len--) {
-    var arg = args[len];
-    res[typeOf(arg)] = arg;
-  }
-  return res;
 }
--- test.js
+++ test.js
@@ -33,11 +33,6 @@ var fixtures = path.join(process.cwd(), 'fixtures');
  */
 
 describe('export-files:', function() {
-  it('should recurse when `true` is passed', function() {
-    var files = exportFiles(fixtures, true);
-    files.should.have.properties('a', 'b', 'c', 'd', 'i', 'j', 'k');
-  });
-
   it('should return the modules from the given cwd', function() {
     var files = exportFiles(fixtures);
     files.should.have.properties('a', 'b', 'c', 'd');
@@ -48,95 +43,5 @@ describe('export-files:', function() {
     var files = exportFiles(fixtures);
     files.should.have.properties('a', 'b', 'c', 'd');
     files.should.not.have.properties('i', 'j', 'k');
-  });
-});
-
-describe('glob/regex patterns', function() {
-  it('should support glob patterns for filtering files:', function() {
-    var files = exportFiles(fixtures, '**/*.json');
-    files.should.eql({ a: { desc: 'aaa', name: 'a.json' }, b: { bb: 'bbb' } });
-  });
-
-  it('should support regex for filtering files:', function() {
-    var re = /\.json/;
-    var files = exportFiles(fixtures, re);
-    files.should.eql({ a: { desc: 'aaa', name: 'a.json' }, b: { bb: 'bbb' } });
-  });
-});
-
-describe('callback function', function() {
-  it('should allow a callback function to read files', function() {
-    var files = exportFiles(fixtures, function (fp) {
-      return require(fp)
-    });
-    files.should.have.properties('a', 'b', 'c', 'd');
-    files.should.not.have.properties('i', 'j', 'k');
-  });
-
-  it('should use the callback to read files and rename keys from options:', function() {
-    var files = exportFiles(fixtures, {renameKey: rename}, function (fp) {
-      return require(fp);
-    });
-
-    files.should.have.properties('a.js', 'b.js', 'c.js', 'd.js');
-    files.should.not.have.properties('i.js', 'j.js', 'k.js');
-  });
-});
-
-describe('custom filters', function() {
-  it('`.css` filter with recurse `true`', function() {
-    var files = exportFiles(fixtures, true, {filter: filter(/\.css$/)});
-    files.should.have.properties('a', 'e');
-    files.a.should.equal('div span {\n  color: red;\n}');
-    files.e.should.equal('#navbar {\n  color: black;\n}');
-  });
-
-  it('should dynamically choose the reader:', function() {
-    var files = exportFiles(fixtures, false, {
-      read: require('file-reader').file
-    });
-
-    files.should.have.properties('a', 'b');
-  });
-
-  it('should dynamically choose the reader:', function() {
-    var files = exportFiles(fixtures + '/nested', false, {
-      read: require('file-reader').file
-    });
-
-    files.should.have.properties('e', 'f', 'g');
-  });
-
-  it('should use a read function and `.yml` filter with recurse `false`', function() {
-    var files = exportFiles(fixtures, false, {
-      read: function(fp) {
-        return require('read-yaml').sync(fp);
-      },
-      filter: filter(/\.ya?ml$/)
-    });
-
-    files.should.have.properties('a', 'b');
-    files.a.should.have.properties('name','desc');
-
-    files.b.should.have.property('bb');
-    files.b.bb.should.equal('bbb');
-  });
-
-  it('should use a `.json` filter with recurse `true`', function() {
-    var files = exportFiles(fixtures, true, {
-      filter: filter(/\.json$/)
-    });
-
-    files.should.have.properties('a', 'b', 'e', 'k');
-    files.a.should.have.properties('name','desc');
-
-    files.b.should.have.property('bb');
-    files.b.bb.should.equal('bbb');
-
-    files.e.should.have.property('ee');
-    files.e.ee.should.equal('eee');
-
-    files.k.should.have.property('kk');
-    files.k.kk.should.equal('kkk');
   });
 });
